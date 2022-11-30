9a14dff5f7d6b955d7240fdeacbe9665a8bd5111
RioRocker97 <changsmarter@gmail.com>
2022-11-30 18:01:29 +0700

    upgrade to 2.0

--- diff-1-4.py
+++ diff-1-4.py
@@ -8,7 +8,7 @@ parser.add_argument('filename',action='store',type=str)
 
 args = parser.parse_args()
 
-model_id = 'CompVis/stable-diffusion-v1-4'
+model_id = 'stabilityai/stable-diffusion-2'
 device = 'cuda'
 
 pipe = StableDiffusionPipeline.from_pretrained(model_id,torch_dtype=torch.float16,revision="fp16",use_auth_token=True)
--- txt2img.py
+++ txt2img.py
@@ -1,16 +1,17 @@
 import torch
 from torch import autocast
-from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionPipeline,EulerDiscreteScheduler
 from io import BytesIO
 from img_storage import upload_to_bucket
 from lang_translator import translate
-MODEL_ID = 'CompVis/stable-diffusion-v1-4'
+MODEL_ID = 'stabilityai/stable-diffusion-2'
 DEVICE = 'cuda'
-PIPE = StableDiffusionPipeline.from_pretrained(MODEL_ID,torch_dtype=torch.float16,revision="fp16",use_auth_token=True).to(DEVICE)
+SCHEDULER = EulerDiscreteScheduler.from_pretrained(MODEL_ID, subfolder="scheduler")
+PIPE = StableDiffusionPipeline.from_pretrained(MODEL_ID,scheduler=SCHEDULER,torch_dtype=torch.float16,revision="fp16",use_auth_token=True).to(DEVICE)
 
 def txt2img(prompt:str):
     with autocast(DEVICE):
-        image = PIPE(prompt)["sample"][0]
+        image = PIPE(prompt,width=512,height=512).image[0]
         image.convert('RGB')
         byte_img = BytesIO()
         image.save(byte_img,format='PNG')
